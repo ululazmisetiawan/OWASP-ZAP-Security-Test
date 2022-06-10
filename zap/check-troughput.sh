0d42d6bf306f767eb7a528bdb9d70e71f424a75b
Ulul Azmi Setiawan <v-ululazmi.setiawan@anteraja.id>
2022-06-10 06:51:53 +0000

    Added check-troughput.sh

---
+++ zap/check-troughput.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+actualThroughput=$1
+expectedThroughput=$2
+
+echo "Actual Throughput: $actualThroughput"
+echo "Expected Throughput: $expectedThroughput"
+echo "====="
+
+float_cmp() {
+    awk "BEGIN{exit(!( $* ))}" <&-
+}
+
+if float_cmp "$actualThroughput >= $expectedThroughput"; then
+  echo "Test Passed because actual throughput is greater or equal than expected throughput"
+else
+  echo "Test Throughput Failed because actual throughput less than expected throughput"
+  exit 1
+  #write-error "Test Failed";
+fi

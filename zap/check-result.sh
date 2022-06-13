2c11d2a63ac3d2907369df7d8989d0d6a3623192
Ulul Azmi Setiawan <v-ululazmi.setiawan@anteraja.id>
2022-06-13 02:29:30 +0000

    Updated check-result.sh

--- zap/check-result.sh
+++ zap/check-result.sh
@@ -14,9 +14,9 @@ float_cmp() {
 }
 
 if float_cmp "$actualResult> $expectedResult"; then
-  echo "Test Throughput FAILED because Actual Total Result greater than Expected Total Result"
+  echo "Result : Test FAILED because Actual Total Result greater than Expected Total Result"
   exit 1
   #write-error "Test Failed";
 else
-  echo "Test PASSED because Actual Total Result is less than Expected Total Result"
+  echo "Result : Test PASSED because Actual Total Result is less than Expected Total Result"
 fi

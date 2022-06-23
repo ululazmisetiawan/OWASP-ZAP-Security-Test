e47213207d344f8593a46a7a2b48762ea9801d1e
Ulul Azmi Setiawan <v-ululazmi.setiawan@anteraja.id>
2022-06-23 03:39:57 +0000

    Updated check-result.sh

--- zap/check-result.sh
+++ zap/check-result.sh
@@ -5,7 +5,7 @@ expectedResult=$2
 actualHigh=$3
 
 echo "======PASSED / FAILED======="
-echo "Actual Total Failed : $(actualHigh)"
+echo "Actual Total Failed : $actualHigh"
 echo "Actual Total Failed : $actualResult"
 echo "Maximal Failed Allowed : $expectedResult"
 
@@ -15,7 +15,11 @@ float_cmp() {
     awk "BEGIN{exit(!( $* ))}" <&-
 }
 
-if float_cmp "$actualResult > $expectedResult"; then
+if "$actualHigh" > 0; then
+  echo "There's HIGH result"
+  exit 1
+  #write-error "Test Failed"
+elif float_cmp "$actualResult > $expectedResult"; then
   echo "Test Failed because Actual Total Failed is greater or equal than Maximal Failed Allowed"
   exit 1
   #write-error "Test Failed";

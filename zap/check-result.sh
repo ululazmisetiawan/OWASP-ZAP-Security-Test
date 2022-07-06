564a25dfd4f3e4e29430b93d3e69c979b4eac5cc
Ulul Azmi Setiawan <v-ululazmi.setiawan@anteraja.id>
2022-07-06 03:51:46 +0000

    Updated check-result.sh

--- zap/check-result.sh
+++ zap/check-result.sh
@@ -19,7 +19,7 @@ if float_cmp "$actualHigh >= $expectedHigh"; then
   echo "There's HIGH result"
   exit 1
   #write-error "Test Failed"
-elif float_cmp "$actualResult > $expectedResult"; then
+elif float_cmp "$actualResult >= $expectedResult"; then
   echo "Test Failed because Actual Total Failed is greater or equal than Maximal Failed Allowed"
   exit 1
   #write-error "Test Failed";

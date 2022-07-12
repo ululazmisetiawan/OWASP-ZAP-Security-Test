9229c06c4a0bcaad9a242118bf14e9b7e2953080
Ulul Azmi Setiawan <v-ululazmi.setiawan@anteraja.id>
2022-07-12 14:35:53 +0000

    Updated check-result.sh

--- zap/check-result.sh
+++ zap/check-result.sh
@@ -8,11 +8,14 @@ actualMedium=$5
 expectedMedium=$6
 actualLow=$7
 expectedLow=$8
+actualInformational=$9
+expectedInformational=$10
 
 echo "======PASSED / FAILED======="
 echo "High Result : $actualHigh"
 echo "Medium Result : $actualMedium"
 echo "Low Result : $actualLow"
+echo "Informational Result : $actualInformational"
 echo "Actual Total Failed : $actualResult"
 echo "Maximal Failed Allowed : $expectedResult"
 
@@ -33,6 +36,10 @@ elif float_cmp "$actualLow >= $expectedLow"; then
   echo "There's Low result"
   exit 1
   #write-error "Test Failed"
+elif float_cmp "$actualInformational >= $expectedInformational"; then
+  echo "There's Informational result"
+  exit 1
+  #write-error "Test Failed";
 elif float_cmp "$actualResult >= $expectedResult"; then
   echo "Test Failed because Actual Total Failed is greater or equal than Maximal Failed Allowed"
   exit 1
